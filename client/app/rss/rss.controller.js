1af6224c5eba9894ebb0f18c7bda295649842b56
northcott-j <northcott.j@chisigma.co>
2017-11-25 01:06:21 -0500

    Working Article feed with shuffle

--- client/app/rss/rss.controller.js
+++ client/app/rss/rss.controller.js
@@ -1,6 +1,15 @@
 'use strict';
 
 angular.module('awd-reflection')
-  .controller('RSSCtrl', function ($scope, $http, $mdDialog) {
+    .controller('RSSCtrl', function ($scope, $http, $mdDialog) {
+        $scope.feeds = [];
+        // Gets a new set of rss articles
+        $scope.update_rss = function () {
+            $http.get("/api/rss/articles")
+                .then(function successCallback(response) {
+                    $scope.feeds = response.data.slice(0,30);
+                })
+        };
 
-  });
+        $scope.update_rss();
+    });
