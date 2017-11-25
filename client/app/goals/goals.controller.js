525e516526cc6b1e4019325c90087ba9c59415e5
northcott-j <northcott.j@chisigma.co>
2017-11-25 00:30:53 -0500

    Working goals inspiration frontend

--- client/app/goals/goals.controller.js
+++ client/app/goals/goals.controller.js
@@ -1,6 +1,27 @@
 'use strict';
 
 angular.module('awd-reflection')
-  .controller('GoalsCtrl', function ($scope, $http, $mdDialog) {
+    .controller('GoalsCtrl', function ($scope, $http, $mdDialog) {
+        $scope.goals = {};
+        $scope.feeds = [];
+        $scope.selected_goal = '';
+        // Gets the search results for a goal
+        $scope.update_feeds = function (goal) {
+            $scope.selected_goal = goal;
+            $http.get("/api/goals/" + goal)
+                .then(function successCallback(response) {
+                    $scope.feeds = response.data;
+                })
+        };
 
-  });
+        // Gets possible goals
+        $scope.update_goals = function () {
+            $http.get("/api/goals")
+                .then(function successCallback(response) {
+                    $scope.goals = response.data;
+                })
+        };
+
+        $scope.update_goals();
+        $scope.update_feeds(1);
+    });
